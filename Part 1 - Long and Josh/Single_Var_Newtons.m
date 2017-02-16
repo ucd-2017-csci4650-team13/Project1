f66ead27b39d562172fb846c367a3bb91457911e
lttrizzle <ltruong1095@gmail.com>
2017-02-15 22:19:27 -0700

    Clean up and reusing function handles
    Using symbolic functions and subs caused some calculations to hang
    again, reverting back to what worked.

--- Part 1 - Long and Josh/Single_Var_Newtons.m
+++ Part 1 - Long and Josh/Single_Var_Newtons.m
@@ -2,37 +2,44 @@
 syms x;
 Tol = 0.00000001;
 %Reading in input as a symbolic variable to use diff
-fxn = input('Give an equation in x: ');
+infxn = input('Give an equation in x: ');
 x = double(input('Enter the initial guess: '));
 r = double(input('Enter the real root: '));
-fprime = diff(fxn);                 % Gets the derivative 
+f = matlabFunction(infxn)  ;            % Equation definition
+fprime = matlabFunction(diff(infxn));   % First-order derivative of f
+%fprime = diff(fxn);                    % Gets the derivative 
 
-i = 0;                              % Tracks the iteration
+i = 0;                                  % Tracks the iteration
 dx = 1;
+fofx = 1;
 pastError = 0;
 
 fprintf('step       x      ei = |xi - r|  ei/(ei-1)^2\n');
 fprintf('----   ---------   -----------   ----------\n');
 
-while (dx > Tol || abs(fx) > Tol)   % While difference between xi+1 and xi is above the tolerance
-    fx = subs(fxn, x);
-    fprimex = subs(fprime, x);
-    if(fprimex == 0 || abs(fprimex) == Inf)
+while (dx > Tol || abs(fofx) > Tol)   % While difference between xi+1 and xi is above the tolerance
+    fofx = f(x);
+    fpofx = fprime(x);
+    if(fpofx == 0 || abs(fpofx) == Inf)
         error('The derivative of the function at %12.8f is 0, try another initial guess', x);
     end
-    xip1 = x - (fx/fprimex);
-    ei = abs(x - r);
-    iterativeError = ei/(pastError)^2;
-    if iterativeError == Inf || iterativeError > 1
+    
+    xip1 = x - fofx/fpofx;              % Gets the next value of x
+    ei = abs(x - r);                    % Gets forward error of current iteration
+    iterativeError = ei/(pastError)^2;  % Error in relation to previous iteration
+    
+    if iterativeError == Inf || iterativeError > 1  % Handles case where result is invalid
         iterativeError = NaN;
     end
+    
     fprintf('%3i %12.8f %12.8f   %12.8f %12.8f\n', i, x, ei, iterativeError, xip1);
+    
     % Prep for next iteration of loop
     pastError = ei;
-    dx = abs(x-xip1);
     x = xip1;
     i = i + 1;
 end
 
-calcError(fxn, fprime, r, x);
-%TODO Relative Errors and Error Magnification
+fprintf('root = %12.8f\n', x);
+calcError(infxn, fprime, r, x);
+fprintf('root %12.8f has m = %12.8f\n', r, getRootMultiplicity(infxn, r));
