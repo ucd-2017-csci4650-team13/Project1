f323195ec22013c4a79b8ff08577b6e2f27790dc
Tish Konz <latishakonz@gmail.com>
2017-02-19 15:31:51 -0700

    Used slicing instead of iterating for loops

--- Part 2 - Sarah and Tish/LU_Decomposition.m
+++ Part 2 - Sarah and Tish/LU_Decomposition.m
@@ -8,90 +8,49 @@ A=[-1, 0, 1;
    -1, 2, 0]
 
 % size of n x n
-[n, n] = size(A)
-col = n
+[n, n] = size(A);
 
 % init U to A
-U = A
+U = A;
 
 % init L to identity
-L = eye(n)
+L = eye(n);
 
 % identity matrix for pivoting
-P = eye(n)
+P = eye(n);
 
-% row contining max value
-max = 0;
-row = 0;
-for i = 1:n
-    if abs(max) < abs(A(i,1))
-        max = A(i,1)
-        row = i
-    end
-end
-
-if row > 1
-    %interchange row with 1
-    temp = A
-    temp_p = P
-    for i = 1:n
-        % move row 1 into the row with max pivot 
-        A(row, i) = A(1, i)
-        % permute Identity matrix
-        P(row, i) = P(1, i)
-        % move max pivot row into row 1 
-        A(1, i) = temp(row, i)
-        % permute Identity matrix
-        P(1, i) = temp_p(row, i)
-    end
-end
-
-U = A  
-% Upper triangular matrix
-for j = 1:n-1 % n-1 = num of rows - the 1st row
-    % check for division by zero
-    if U(j,j) == 0
-        error('zero pivot encountered');
-    end
-    %%%%%%%%%%%%%%DEBUG%%%%%%%%%%%%%%%%%
-    % eliminate col j to put zero in each location below diag
-    % ex. a(j+1, j), a(j+2, j),...a(n,j)
-    % var to keep track of val of pivot
-    row = j
-    for i = j+1:n
-        if abs(U(i, 1)) > abs(U(row, 1))
-        %interchange row i with row j
-        temp_u = U
-        temp_p = P
-            for index = 1:n
-            % move row j into the row i 
-            U(row, index) = U(i, index);   
-            % permute Identity matrix
-            P(row, index) = P(i, index);
-            % move row i into row j 
-            U(i, index) = temp_u(row, index)
-            % permute Identity matrix
-            P(i, index) = temp_p(row, index)
-            end
-        end
-        % row multiplier
-        % TODO A(j,j) == 0 fix
-        multi = U(i, j) / U(j, j)
+for k = 1:n
+    % create matrix pivot
+    %  maximum absolute value from the k thru n rows, k column
+    [pivot i] = max(abs(U(k:n,k)));
+    i = i + k-1;
+    
+    % if i != k
+    if i ~= k 
+        % interchange rows i and k in U
+        % U(k, :) means (row k, all columns in row k)
+        temp = U(k, :);
+        U(k, :) = U(i, :);
+        U(i, :) = temp;
+        % interchange rows i and k in P
+        temp = P(k, :);
+        P(k, :) = P(i, :);
+        P(i, :) = temp;
         
-        % L factor gets multiplier
-        L(i, j) = multi   
-        
-        % subtract multiplier * the row from 
-        for index = j:col
-            U(i, index) = U(i, index) - (multi * U(j, index))
-            U(i, index)
-        end
-        %%%%%%%%%%%%%%BREAKPOINT%%%%%%%%%%%%%%%%%
-        row = row + 1
-    end
+        % Lower triangular array
+        if k >= 2
+            % (k , 1:k-1) means entire row k and columns 1 thru k-1
+            temp = L(k, 1:k-1);
+            L(k, 1:k-1) = L(i, 1:k-1);
+            L(i, 1:k-1) = temp;
+        end %endif
+    end %end outer if ~=
+    % get factors L and U
+    for j = k+1:n
+        % set multiplier in L
+        L(j, k) = U(j, k) / U(k, k);
+        % for all in row j = for all in row j - multiplier*for all in row k
+        U(j, :) = U(j, :) - L(j, k) * U(k, :);
+    end % end gauss elim
 end
-
-
-
-
-
+    
