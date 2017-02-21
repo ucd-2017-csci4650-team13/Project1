6f533c55ca3b189597135d9c91d328bb3ee91a97
lttrizzle <ltruong1095@gmail.com>
2017-02-21 12:07:36 -0700

    Renamed X to xList

--- Part 1 - Long and Josh/Single_Var_FixedPoint.m
+++ Part 1 - Long and Josh/Single_Var_FixedPoint.m
@@ -2,20 +2,20 @@
 % TODO, make into function
 syms x;
 Tol = 0.00000001;   % Stopping criteria
-x = zeros;          % Have x be a list for creating graphs
+xList = zeros;          % Have x be a list for creating graphs
 
 infxn = input('Give an equation in x: ');
 f = matlabFunction(infxn); % Equation definition
 
-x(1) = double(input('Enter the initial guess: '));
+xList(1) = double(input('Enter the initial guess: '));
 dx = 1;
 i = 1;
 
 while(abs(dx) > Tol)
-    x(i+1) = f(x(i));           % Get next x from result of current x
-    dx = abs(x(i+1) - x(i));    % Tracks amount result changed
+    xList(i+1) = f(xList(i));           % Get next x from result of current x
+    dx = abs(xList(i+1) - xList(i));    % Tracks amount result changed
     i = i + 1;
 end
 
-xa = x(i);
+xa = xList(i);
 fprintf('Approximate root = %12.8f\n', xa);
