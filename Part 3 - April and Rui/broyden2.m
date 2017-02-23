e7f5d301d5cbf20f68df6df3e7e4f4c4986c4153
arainh <april.hudspeth@ucdenver.edu>
2017-02-22 18:19:39 -0700

    implementatiof broyden 2 method

---
+++ Part 3 - April and Rui/broyden2.m
@@ -0,0 +1,36 @@
+x_i = input('enter a starting point');
+B = input('enter an initial matrix');
+eqns = input('enter a system of equations');
+number_of_iterations = input('enter the number of iterations');
+
+%evaluate the functions at x
+%{(@(u,v)u^2+v^2-1) ,   (@(u,v)(u-1)^2+v^2-1)}
+x = num2cell(x_i);
+y1 = cellfun(@(t) t(x{:}), eqns);
+x1 = x_i;
+x1 = transpose(x1);
+y1 = transpose(y1);
+
+
+for i=1:number_of_iterations
+    x = x1;
+    y = y1;
+    x1 = x - B*y
+    x_val = num2cell(x1);
+    display(x1);
+    y1 = cellfun(@(t) t(x_val{:}), eqns);
+    y1 = transpose(y1);
+    
+    %calculate new matrix B
+    deltaY = y1 - y;
+    deltaX = x1 - x;
+    p1 = B*deltaX;
+    p2 = deltaY - p1;
+    p3 = p2 * (transpose(deltaX)*B);
+    p4 = transpose(deltaX)*B*deltaX;
+    B = B + p3/p4;
+end
+
+
+
+
