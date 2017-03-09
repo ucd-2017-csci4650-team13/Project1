e8e4eefe4ef1430a9ec8b17291fa5af3df00086e
arainh <april.hudspeth@ucdenver.edu>
2017-03-09 12:28:17 -0700

    fixed error calculation

--- Part 3 - April and Rui/find_error.m
+++ Part 3 - April and Rui/find_error.m
@@ -1,16 +1,22 @@
-function e=find_error(r, xa, eqns)
+function e=find_error(b, A, xa, x)
 %forward error
-forward = abs(r - xa);
+forward = norm((x - xa),inf);
 formatSpec = 'Forward error is %4.2f';
 fprintf(formatSpec,forward);
 
 %backward error
-x = num2cell(xa);
-backward = cellfun(@(t) t(x{:}), eqns);
-backward = 
+r = b - A*xa;
+
+backward = norm(r, inf);
 formatSpec = 'Backward error is %4.2f';
 fprintf(formatSpec,backward);
 
 %magnification error
+rel_forward = forward/(norm(x,inf));
+rel_backward = backward/(norm(b,inf));
+
+magnification_err = rel_forward/rel_backward;
+formatSpec = 'Magnification error is %4.2f';
+fprintf(formatSpec, magnification_err);
 
 end
