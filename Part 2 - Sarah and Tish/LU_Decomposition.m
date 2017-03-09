2908b4f686009de98f4d63c7efd351ef351a6685
Tish Konz <latishakonz@gmail.com>
2017-03-08 18:39:41 -0700

    Documentation, symmetry check, opCount added

--- Part 2 - Sarah and Tish/LU_Decomposition.m
+++ Part 2 - Sarah and Tish/LU_Decomposition.m
@@ -1,10 +1,22 @@
-clc;
-clear all;
-close all;
+% This function performs the LU factorization of a symmetric matrix.
+% The function checks for singularity by checking for a zero pivot.
+% The function also does a partial pivoting protocol to prevent swamping.
+% This function also creates a permutation matrix to help with backsolving.
+%   Input: Symmetric matrix and solution vector 
+%   Output: Lower Triangular factor
+%           Upper Triangular factor
+%           Permutation matrix
+%           Solution Vector x
 
 % input from user for now. may need to update.
-A = input('enter an initial matrix');
-b = input('enter an initial vector');
+A = input('enter an initial matrix: \n');
+b = input('enter an initial vector: \n');
+
+% test for symmetry
+tf = issymmetric(A)
+if (tf == 0)
+    fprintf('The matrix you have entered is not symmetric and will not factor.');
+end
 
 % % % matrix A
 % A=[-1, 0, 1;
@@ -30,15 +42,17 @@ for k = 1:n
     %  maximum absolute value from the k thru n rows, k column
     [pivot i] = max(abs(U(k:n,k)));
     i = i + k-1;
-    
+    opCount = opCount + 1;
     % if i != k
     if i ~= k 
         % interchange rows i and k in U
         % U(k, :) means (row k, all columns in row k)
+        opCount = opCount + 3;
         temp = U(k, :);
         U(k, :) = U(i, :);
         U(i, :) = temp;
         % interchange rows i and k in P
+        opCount = opCount + 3;
         temp = P(k, :);
         P(k, :) = P(i, :);
         P(i, :) = temp;
@@ -49,23 +63,39 @@ for k = 1:n
             temp = L(k, 1:k-1);
             L(k, 1:k-1) = L(i, 1:k-1);
             L(i, 1:k-1) = temp;
+            opCount = opCount + 3;
         end %endif
     end %end outer if ~=
     % get factors L and U
     for j = k+1:n
         % set multiplier in L
-        L(j, k) = U(j, k) / U(k, k); % flop count
+        L(j, k) = U(j, k) / U(k, k); 
+        opCount = opCount + 1;
         % for all in row j = for all in row j - multiplier*for all in row k
-        U(j, :) = U(j, :) - L(j, k) * U(k, :)
+        U(j, :) = U(j, :) - L(j, k) * U(k, :);
+        opCount = opCount + 1;
     end % end gauss elim
 end
 
-% solving for c
+%solve for c
 c = [];
 inv(L)
-Pb = P*b
-c = L\Pb
+Pb = P*b;
+c = L\Pb;
+opCount = opCount + n^2;
 
-% solving for x
+%solve for x
 x = [];
-x = U\c
+x = U\c;
+opCount = opCount + n^2;
+
+fprintf('Number of Operations = %d\n', opCount);
+fprintf('Lower Triangular = \n');
+L
+fprintf('Upper Triangular = \n');
+U
+fprintf('Permutation Matrix = \n');
+P
+fprintf('Solution vector = \n');
+x
+
